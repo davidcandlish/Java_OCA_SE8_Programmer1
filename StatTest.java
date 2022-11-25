22810b3551e8b6750e0eba717e63ce99b477b31b
David Candlish <107430066+davidcandlish@users.noreply.github.com>
2022-11-25 11:03:43 +0000

    Add files via upload

---
+++ StatTest.java
@@ -0,0 +1,10 @@
+public class StatTest {
+public static void main(String[] args) {
+
+double a = (double)(8/(double)13)*(double)100;
+
+System.out.println((8/13)*100);
+System.out.println(a);
+
+}
+}
